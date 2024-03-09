8868301e97599fe711b6cd67a998dcc151c65711
F7MH2R <skyicef7@gmail.com>
2024-03-09 12:19:13 -0600

    Modelado

--- PARCIAL1A/Models/LibroContex.cs
+++ PARCIAL1A/Models/LibroContex.cs
@@ -4,6 +4,10 @@ namespace PARCIAL1A.Models
 {
     public class LibroContex:DbContext
     {
+        public LibroContex(DbContextOptions<LibroContex> options) : base(options)
+        {
+
+        }
 
     }
 }
---
+++ PARCIAL1A/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PARCIAL1A.Models
+{
+    public class Post
+    {
+        [Key]
+        public int id { get; set; }
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public int AutorId { get; set; }
+
+    }
+}
